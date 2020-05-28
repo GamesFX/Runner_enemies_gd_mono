e11e13b69ffa6492cb4b41fba69f0271d99f43fc
Ushiosan23 <haloleyendee@outlook.com>
2020-05-28 04:21:16 -0500

    feat(core): Create custom node FactoryNode
    Used to instance new objects in specific position, rotation, and other properties

---
+++ Scripts/CustomNodes/FactoryNode.cs
@@ -0,0 +1,65 @@
+﻿using Godot;
+
+namespace RunnerEnemyGD.Scripts.CustomNodes {
+
+	/// <summary>
+	/// Custom node to create nodes
+	/// </summary>
+	public class FactoryNode : Position2D {
+
+		/// <summary>
+		/// Instanced node.
+		/// </summary>
+		[Signal]
+		public delegate void InstancedNode(Node node);
+
+		/// <summary>
+		/// Target scene to load
+		/// </summary>
+		[Export]
+		public PackedScene TargetScene {
+			get; protected set;
+		}
+
+		/// <summary>
+		/// Create node instance
+		/// </summary>
+		/// <returns></returns>
+		public Node2D MakeInstance() {
+			Node2D result = TargetScene.Instance() as Node2D;
+			ConfigureNode(result);
+
+			EmitSignal(nameof(InstancedNode), result as Node);
+			return result;
+		}
+
+		/// <summary>
+		/// Get node instance typed
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
+		public T MakeInstanceTyped<T>() where T : Object {
+			Object result = TargetScene.Instance();
+			try {
+				ConfigureNode(result as Node2D);
+				EmitSignal(nameof(InstancedNode), result as Node);
+				return result as T;
+			} catch {
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Set configuration node
+		/// </summary>
+		/// <param name="node"></param>
+		private void ConfigureNode(Node2D node) {
+			node.Position = Position;
+			node.RotationDegrees = RotationDegrees;
+			node.Scale = Scale;
+		}
+
+	}
+
+
+}
